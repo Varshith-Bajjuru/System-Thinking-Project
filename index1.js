3c6d81f5c6e451bbf2312d440499a40ff48af279
Varshith-Bajjuru <165387944+Varshith-Bajjuru@users.noreply.github.com>
2024-10-06 21:48:55 +0530

    Added pics

--- index1.js
+++ index1.js
@@ -12,9 +12,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/public/index1.html'); 
 });
 
 app.listen(port, () => {
-    console.log(`Server running on the port = ${port}`);
+    console.log(`Server running on port ${port}`);
 });
