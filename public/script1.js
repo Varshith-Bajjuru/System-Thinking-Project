ca070935dbe75975d807e8d1864bc99c10880785
Jagadeeswar <manyamjagadeeswar7989@gmail.com>
2024-10-16 19:47:18 +0530

    Updtaed the files

--- public/script1.js
+++ public/script1.js
@@ -1,9 +1,13 @@
 'use strict';
-
+// sliding buttons
 const slideContainer = document.querySelector('.slider-container');
 const slides = document.querySelectorAll('.slider-container .slide');
 const btnleft = document.querySelector('.slider-controls .btn_left');
 const btnright = document.querySelector('.slider-controls .btn_right');
+
+// FAQs buttons
+const FAQs = document.querySelectorAll('.FAQs-section-container');
+const questions = document.querySelectorAll('.Question');
 
 let currentIndex = 0;
 const translateSlide = function (index) {
@@ -22,3 +26,13 @@ const showPreviousSlide = function () {
 btnleft.addEventListener('click', showPreviousSlide);
 btnright.addEventListener('click', showNextSlide);
 translateSlide(currentIndex);
+
+// FAQs events handling
+
+questions.forEach(question => {
+  question.addEventListener('click', function (event) {
+    event.stopPropagation();
+    const Answer = question.nextElementSibling;
+    Answer.classList.toggle('hidden');
+  });
+});
