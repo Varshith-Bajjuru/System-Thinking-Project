7ad0865516ea602bbcc3606fb55e720faa289dbb
Varshith-Bajjuru <165387944+Varshith-Bajjuru@users.noreply.github.com>
2024-10-21 22:55:32 +0530

    Added blogs page

--- index1.js
+++ index1.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -14,6 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index1.html'); 
 });
+
+app.get("/blog",(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname + '/public/blog.html'));
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
