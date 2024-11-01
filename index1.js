60b1778d9f3db0ab0705da9a5df34cd3b41f26dd
Varshith-Bajjuru <165387944+Varshith-Bajjuru@users.noreply.github.com>
2024-11-01 16:27:50 +0530

    Updated all routes

--- index1.js
+++ index1.js
@@ -13,12 +13,24 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index1.html");
+  res.sendFile(__dirname + "/public/main.html");
 });
 
 app.get("/blog", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname + "/public/blog.html"));
+  res.sendFile(path.join(__dirname + "/public/blog.html"));
 });
+
+app.get("/home",(req,res)=>{
+  res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get("/Courses",(req,res)=>{
+  res.sendFile(__dirname + "/public/Courses.html")
+})
+
+app.get("/about",(req,res)=>{
+  res.sendFile(__dirname+"/public/about.html")
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
