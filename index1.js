72f8a44ca607522e59ea8729eeb778c717bae5a7
Varshith-Bajjuru <165387944+Varshith-Bajjuru@users.noreply.github.com>
2024-11-03 15:15:59 +0530

    modified all files

--- index1.js
+++ index1.js
@@ -8,6 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,12 +25,17 @@ app.get("/home",(req,res)=>{
   res.sendFile(__dirname + "/public/main.html")
 })
 
+
 app.get("/Courses",(req,res)=>{
   res.sendFile(__dirname + "/public/Courses.html")
 })
 
 app.get("/about",(req,res)=>{
   res.sendFile(__dirname+"/public/about.html")
+})
+
+app.get('/images',(req,res)=>{
+  res.sendFile(__dirname+"/public/FAQs.html")
 })
 
 app.listen(port, () => {
